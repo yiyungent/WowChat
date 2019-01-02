108b3426f689d0c29f6e58084ba4d8f674b1ea94
yiyun <yiyungent@gmail.com>
2019-01-03 00:29:35 +0800

    Add three Model

---
+++ src/WowChat.Model/Priv_info.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WowChat.Model
+{
+    /// <summary>
+	/// priv_info:实体类(属性说明自动提取数据库字段的描述信息)
+	/// </summary>
+	[Serializable]
+    public partial class Priv_info
+    {
+        public Priv_info()
+        { }
+        #region Model
+        private int _id;
+        private string _name;
+        private int _status = 0;
+        private string _remark;
+        /// <summary>
+        /// 
+        /// </summary>
+        public int id
+        {
+            set { _id = value; }
+            get { return _id; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string name
+        {
+            set { _name = value; }
+            get { return _name; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public int status
+        {
+            set { _status = value; }
+            get { return _status; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string remark
+        {
+            set { _remark = value; }
+            get { return _remark; }
+        }
+        #endregion Model
+    }
+}
---
+++ src/WowChat.Model/User_info.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WowChat.Model
+{
+    /// <summary>
+	/// user_info:实体类(属性说明自动提取数据库字段的描述信息)
+	/// </summary>
+	[Serializable]
+    public partial class User_info
+    {
+        public User_info()
+        { }
+        #region Model
+        private int _id;
+        private int _group_id;
+        private string _name;
+        private string _password;
+        private string _email;
+        private string _phone;
+        private string _avatar;
+        private DateTime _create_time;
+        private DateTime _login_time;
+        private string _login_ip;
+        private int _logins = 0;
+        private int _score = 0;
+        private int _status = 0;
+        private string _remark;
+        /// <summary>
+        /// 
+        /// </summary>
+        public int id
+        {
+            set { _id = value; }
+            get { return _id; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public int group_id
+        {
+            set { _group_id = value; }
+            get { return _group_id; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string name
+        {
+            set { _name = value; }
+            get { return _name; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string password
+        {
+            set { _password = value; }
+            get { return _password; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string email
+        {
+            set { _email = value; }
+            get { return _email; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string phone
+        {
+            set { _phone = value; }
+            get { return _phone; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string avatar
+        {
+            set { _avatar = value; }
+            get { return _avatar; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public DateTime create_time
+        {
+            set { _create_time = value; }
+            get { return _create_time; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public DateTime login_time
+        {
+            set { _login_time = value; }
+            get { return _login_time; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string login_ip
+        {
+            set { _login_ip = value; }
+            get { return _login_ip; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public int logins
+        {
+            set { _logins = value; }
+            get { return _logins; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public int score
+        {
+            set { _score = value; }
+            get { return _score; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public int status
+        {
+            set { _status = value; }
+            get { return _status; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string remark
+        {
+            set { _remark = value; }
+            get { return _remark; }
+        }
+        #endregion Model
+    }
+}
