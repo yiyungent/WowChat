ca8320371bb7703ca2c8e091d34777450f81ae4c
yiyun <yiyungent@gmail.com>
2019-01-28 17:06:23 +0800

    路由 URL小写化

--- src/WowChat.Web/App_Start/RouteConfig.cs
+++ src/WowChat.Web/App_Start/RouteConfig.cs
@@ -18,6 +18,7 @@ namespace WowChat.Web
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+            routes.LowercaseUrls = true;
         }
     }
 }
